f83283cd1d2d3a84da3d42bdb66cbff86c622a52
ovnisoftware <info@ovnisoftware.com>
2015-09-09 10:47:39 -0400

    Initial commit
    Implementing a linked list as a stack

---
+++ LinkedListPuzzles/LinkedListPuzzles/LinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPuzzles
+{
+    //Implement a LinkedList as a stack (LIFO / Last In, First Out)
+    //Implement a singly linked list as a stack (Push, Pop, Clear, Count, Peek)
+    public class LinkedListStack
+    {
+        private Node Head;
+        public int Count { get; private set; }
+
+        public LinkedListStack()
+        {
+            this.Head = null;
+        }
+
+        //We are adding before the head to implement as a stack (Each new node added becomes the head)
+        public void Push(object x)
+        { 
+            Node n = new Node(x);
+            n.Next = Head;
+            this.Head = n;
+            Count++;
+        }
+
+        public object Pop()
+        { 
+            object result;
+            if (Head == null)
+                //This is how the C# Stack implementation in System.Collections handles popping an empty stack
+                throw new InvalidOperationException("Stack empty");
+            else
+            {
+                result = Head.Data;
+                this.Head = Head.Next;
+                return result;
+            }
+        }
+
+        //Returns the next object in the stack while not popping it off the stack
+        public object Peek()
+        {
+            return Head.Data;
+        }
+
+        //Clears the Stack, resets the count
+        public void Clear()
+        {
+            this.Head = null;
+            Count = 0;
+        }
+        ////Call in Program.cs to test
+        //LinkedListStack list = new LinkedListStack();
+        //list.Push("pushed first");
+        //list.Push("pushed last");
+        //Console.WriteLine(list.Count);
+        //Console.WriteLine(list.Pop());
+        ////Output should be:
+        ////2
+        ////pushed last
+    }
+}
---
+++ LinkedListPuzzles/LinkedListPuzzles/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPuzzles
+{
+    //Stores the object and pointer to the next node in the list
+    public class Node
+    {
+        public object Data { get; private set; }
+        public Node Next { get; set; }
+
+        public Node(object x)
+        {
+            this.Data = x;
+            this.Next = null;
+        }
+    }
+}
